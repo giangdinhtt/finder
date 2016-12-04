28823e269f2c858c5c797db35533f8a09c56ea33
Giang Dinh <giangdinhtt@gmail.com>
2016-12-04 23:40:11 +0700

    Bring index.html to root

--- api/find.php
+++ api/find.php
@@ -1,5 +1,9 @@
 <?php
-$arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
 
-echo json_encode($arr);
+$query = isset($_POST['q']) ? $_POST['q'] : null;
+$data = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
+
+header('Content-Type: application/json');
+echo json_encode($data);
+
 ?>
