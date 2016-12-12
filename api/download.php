3a703a647c1958c91309b83f115b45993a1f4c96
Giang Dinh <giangdinhtt@gmail.com>
2016-12-13 01:54:33 +0700

    Add download.pph

--- api/download.php
+++ api/download.php
@@ -38,23 +38,21 @@ if ($result->num_rows > 0) {
     $host_password = $row["password"];
 }
 
-//$result = $conn->query("SELECT * FROM files f WHERE f.id = " . $file_id . ";");
-
 // Copy file from remote host via FTP
 $script = "/home/pi/Downloads/winexe-winexe-waf/source/build/winexe -U ";
 $script .= $host_user . "%" . $host_password . " //" . $host;
-$script .= " \"cmd /C cd %temp% ";
-$script .= "&echo pi>%temp%\\ftp1.txt";
-$script .= "&echo raspberry>>%temp%\\ftp1.txt";
-$script .= "&echo bin>>%temp%\\ftp1.txt";
-$script .= "&echo cd /tmp>>%temp%\\ftp1.txt";
-$script .= "&echo put \"" . str_replace('\\', '\\\\', $remote_path) . "\">>%temp%\\ftp1.txt";
-$script .= "&echo bye>>%temp%\\ftp1.txt";
-$script .= "& ftp -s:%temp%\\ftp1.txt 192.168.1.3\"";
+$script .= " \"cmd /C ";
+$script .= "echo pi>ftp_pi.txt";
+$script .= "&echo raspberry>>ftp_pi.txt";
+$script .= "&echo bin>>%temp%\\ftp_pi.txt";
+$script .= "&echo cd /tmp>>%temp%\\ftp_pi.txt";
+$script .= "&echo put " . $remote_path . ">>ftp_pi.txt";
+$script .= "&echo bye>>ftp_pi.txt";
+$script .= "& ftp -s:ftp_pi.txt 192.168.1.3\"";
 
-echo $script;
+//echo $script;
 $output = shell_exec($script);
-echo $output;
+//echo $output;
 
 // Send file to browser
 $file = "/tmp/" . $file_id;
